3abed7605a3dde4f0ea4e14b56a3e8ece5289b46
Robert Grimes <rjgrimes@gmail.com>
2020-10-07 14:38:10 -0500

    Adjusted script for detection of clicks

--- assets/mainScript.js
+++ assets/mainScript.js
@@ -1,49 +1,21 @@
 var entertainMe = document.getElementById(".card-actionB");
 var somethingActive = document.getElementById(".card-actionA");
 var supriseMe = document.getElementById(".card-actionC");
-var mapMe = document.getq(".zip-code");
-
-function setVisibility(entertainMe) {
-
-
-    document.addEventListener("click", entertainMe);
-    document.addEventListener("click", mapMe);
-
-    function myFunction() {
-        var x = document.getElementById(".card-actionB");
-        if (x.style.display === ".zip-code") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      }
-
-
-    //   hidden element - zip - show when button is clicked
-    function myFunction() {
-        var x = document.getElementById("myDIV");
-        if (window.getComputedStyle(x).display === "none") {
-          // Do something..
-        }
-      }
-// }
-// function setVisibility(objectID, state) { 
-//     var object = document.getElementById(objectID); object.style.visibility = state; 
-// } function toggleVisibility(objectID) { 
-//     var object = document.getElementById(objectID); 
-//     state = object.style.visibility; 
+var mapMe = document.querySelector(".zip-code");
 
 
 
-
-
-
-    // onclick="setVisibility('.card-actionB', 'hidden'); return false;" 
-    // href="#">mapMe</a> | <a      
-
-
-
-
-
-    // if (state == 'visible') object.style.visibility = 'hidden'; 
-    // else object.style.visibility = 'visible');
+$(document).on("click", function () {
+    if ($(this).attr("class") == "Active") {
+        console.log("Action Button");
+    }
+    else if ($(this).attr("class") == "Entertain") {
+        console.log("Entertain Button");
+    }
+    else if ($(this).attr("class") == "Anything") {
+        console.log("Anything Button");
+    }
+    else {
+        console.log("No button clicked");
+    }
+})
