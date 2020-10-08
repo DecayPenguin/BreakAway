8dba5f5124453d7140bbe9553d3758b0792e7666
Robert Grimes <rjgrimes@gmail.com>
2020-10-08 10:58:24 -0500

    Added front end functionality for the 3 main buttons and reset button

--- assets/mainScript.js
+++ assets/mainScript.js
@@ -1,21 +1,39 @@
-var entertainMe = document.getElementById(".card-actionB");
-var somethingActive = document.getElementById(".card-actionA");
-var supriseMe = document.getElementById(".card-actionC");
-var mapMe = document.querySelector(".zip-code");
+//#region Variable Declaration
+var activeBTN = $("#active");
+var entertainBTN = $("#entertain");
+var foodBTN = $("#wineDine");
 
 
+//#endregion
 
-$(document).on("click", function () {
-    if ($(this).attr("class") == "Active") {
-        console.log("Action Button");
+//#region Function Definitions
+function selectionBtn() {
+    console.log(this);
+    if ($(this).attr("id") == "active") {
+        entertainBTN.addClass("hide");
+        foodBTN.addClass("hide");
     }
-    else if ($(this).attr("class") == "Entertain") {
-        console.log("Entertain Button");
+    else if ($(this).attr("id") == "entertain") {
+        activeBTN.addClass("hide");
+        foodBTN.addClass("hide");
     }
-    else if ($(this).attr("class") == "Anything") {
-        console.log("Anything Button");
+    else if ($(this).attr("id") == "wineDine") {
+        activeBTN.addClass("hide");
+        entertainBTN.addClass("hide");
     }
     else {
-        console.log("No button clicked");
+        activeBTN.removeClass("hide");
+        entertainBTN.removeClass("hide");
+        foodBTN.removeClass("hide");
     }
-})
+}
+
+
+
+
+//#endregion
+
+//#region Event Listeners
+$(document).on("click", "button", selectionBtn);
+
+//#endregion
