52ace575bec7f9c2984f23eb5f06187f633e5e42
Robert Grimes <rjgrimes@gmail.com>
2020-10-07 21:16:08 -0500

    Map now fully functiona, search for places semi-functional, need to work to get a LIST of results rather than the closest result according to google

--- assets/mainScript.js
+++ assets/mainScript.js
@@ -14,20 +14,24 @@ var searchFor = "";
 
 //#region Function Definitions
 
+// reverse lookup for lat/long of address or zipcode user gives the application
 function geoCode(search) {
     queryURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${search}&key=${googleAPI}`;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (getGeo) {
-        console.log(getGeo);
+        // console.log(getGeo);
+        // converts result getGeo into variable lat/long for use elsewhere
         lat = getGeo.results[0].geometry.location.lat;
         long = getGeo.results[0].geometry.location.lng;
-        console.log("Coords: " + lat + ", " + long);
+        // console.log("Coords: " + lat + ", " + long);
+        // calls map to be displayed
         initMap(lat, long);
     })
 }
 
+// Displays map based on the getGeo result from the function geoCode
 function initMap(lat, long) {
     const myLocation = new google.maps.LatLng(lat, long);
     infowindow = new google.maps.InfoWindow();
@@ -36,20 +40,41 @@ function initMap(lat, long) {
         zoom: 15,
     });
 
-    var marker = new google.maps.Marker({position: myLocation, map: map});
-    // const request = {
-    //     query: "Museum of Contemporary Art Australia",
-    //     fields: ["name", "geometry"],
-    // };
-    // service = new google.maps.places.PlacesService(map);
-    // service.findPlaceFromQuery(request, (results, status) => {
-    //     if (status === google.maps.places.PlacesServiceStatus.OK) {
-    //         for (let i = 0; i < results.length; i++) {
-    //             createMarker(results[i]);
-    //         }
-    //         map.setCenter(results[0].geometry.location);
-    //     }
-    // });
+    // Displays marker on current location
+    // var marker = new google.maps.Marker({position: myLocation, map: map});
+}
+
+// function for finding a type of result using Google Places API
+function searchArea(request) {
+    console.log(request);
+    
+    requestOBJ = {
+        query: request,
+        fields: ["name", "geometry"]
+    }
+    map = new google.maps.Map(document.getElementById("map"));
+    service = new google.maps.places.PlacesService(map);
+    service.findPlaceFromQuery(requestOBJ, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (let i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+            map.setCenter(results[0].geometry.location);
+        }
+        console.log(results);
+    });
+}
+
+// creates a marker on each result created from the searchArea function
+function createMarker(place) {
+    const marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location
+    });
+    google.maps.event.addListener(marker, "click", () => {
+        infowindow.setContent(place.name);
+        infowindow.open(map);
+    })
 }
 
 
@@ -67,6 +92,10 @@ $("#searchBtn").click(function () {
         // getGeo(search);
         // generateMap(search);
         geoCode(search);
+
+        // testing purposes: hardcoded search item
+        searchFor = "Food";
+        searchArea(searchFor);
     }
 })
 //#endregion
